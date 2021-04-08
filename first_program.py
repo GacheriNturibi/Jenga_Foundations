512271ae9a15f944f5d616d7aaa07998fbf83d4f
GacheriNturibi <gacherinturibi@gmail.com>
2021-04-08 20:08:55 +0300

    This is the first program

---
+++ first_program.py
@@ -0,0 +1,10 @@
+# Program to calculate the sum of two numbers
+
+def main():
+    x = int(input("Enter a number: "))
+    y = int(input("Enter another number: "))
+
+    sum = x+y
+
+    print(sum)
+main()    
